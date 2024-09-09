0ab99a67df613eb8c82b1061cdc4d01f00336834
Frank Casanova <frankcasanova.info@gmail.com>
2024-09-09 12:36:02 +0200

    showing latest post on sidebar added

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post_djb.objects.count()
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post_djb.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
