0881d256cc4a66bc5d8b83c4ebae8cde46e36a3d
Frank Casanova <frankcasanova.info@gmail.com>
2024-09-10 16:51:26 +0200

    conf to deploy on vercel

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app"]
 
 SITE_ID = 1
 
