193503b0f456378e074ff8bb050c961a809ffe23
Frank Casanova <frankcasanova.info@gmail.com>
2024-09-10 18:25:30 +0200

    Changes to be committed: 	modified:   dj_blog/__pycache__/settings.cpython-312.pyc 	modified:   dj_blog/settings.py

--- dj_blog/settings.py
+++ dj_blog/settings.py
@@ -130,8 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# If you're using Whitenoise (recommended for Vercel deployments)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 
